02931685ea7232cbe095a6dc9727943afc9c9658
joaogomes456 <pontesjoao80@hotmail.com>
2022-11-24 15:18:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cad_User_Prod',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
